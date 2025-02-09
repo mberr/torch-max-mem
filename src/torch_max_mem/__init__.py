87284a73e88a1f35c9c447bc2faabc6990f7d5c6
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-02-09 11:47:08 +0100

    Modernize (#22)

--- src/torch_max_mem/__init__.py
+++ src/torch_max_mem/__init__.py
@@ -1,9 +1,8 @@
-
 """Maximize memory utilization with PyTorch."""
 
 from .api import MemoryUtilizationMaximizer, maximize_memory_utilization
 
 __all__ = [
-    "maximize_memory_utilization",
     "MemoryUtilizationMaximizer",
+    "maximize_memory_utilization",
 ]
