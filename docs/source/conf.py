0180fc88b1ec344d18caa07b0a5ca9679205f0d8
Max Berrendorf <max.berrendorf@gmail.com>
2026-08-26 20:34:20 +0200

    Bump version: 0.1.6 → 0.1.7-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = f"{datetime.now(tz=timezone.utc).date().year}, Max Berrendorf"
 author = "Max Berrendorf"
 
 # The full version, including alpha/beta/rc tags.
-release = "0.1.6"
+release = "0.1.7-dev"
 
 # The short X.Y version.
 parsed_version = re.match(
